a8217fba0917d3d9c21dff5d942d1b4ec09aa45c
JeanCarlosDV <102921906+JeanCarlosDV@users.noreply.github.com>
2022-08-23 15:18:29 -0300

    Update 01.js

--- PraticandoExercicios/01.js
+++ PraticandoExercicios/01.js
@@ -1,5 +1,8 @@
+/* Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.*/
+
 function operacoesMat(a, b) {
     console.log(a+b, a-b, a*b, a/b);
 }
 
-operacoesMat(1, 2)
+operacoesMat(7, 8)
